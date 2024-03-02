a9d88a85c6d0567910ad55614c6ae2a64d874bd7
zaitrarrio <zaitrarrio@gmail.com>
2024-03-02 15:08:39 -0600

    refactor: additional refactoring on filenames for consistency in Python api

--- assess.py
+++ assess.py
@@ -1,4 +1,4 @@
-from sharpness_python_api import run_sharpness_assessment
+from sharpness_assessment import run_sharpness_assessment
 
 if __name__ == '__main__':
     run_sharpness_assessment("./images/fuzzy.jpg", "./images/out", 0.5)
