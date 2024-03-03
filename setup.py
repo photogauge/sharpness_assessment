95364b74061f5da0efc6af982baa3ce999398f3e
zaitrarrio <zaitrarrio@gmail.com>
2024-03-03 17:38:03 -0600

    chore: there is really no need to run a build for a source distribution so disable

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     cmake_install_dir=python_packages_root + "/" + packages[0],
     # CMake must be used allways, otherwise C++ dependencies won't be installed
     # ! setup_requires=["cmake"] should not be used, as it causes `vcpkg` to fail
-    cmake_with_sdist=True,
+    cmake_with_sdist=False,
     # Signal cmake to use `vcpkg`
     cmake_args=["-DBUILD_TESTS=OFF"],
 )
