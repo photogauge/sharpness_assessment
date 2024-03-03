91889f5238d8013bea45aaf9cb3219f28419c54c
zaitrarrio <zaitrarrio@gmail.com>
2024-03-03 09:24:38 -0600

    chore: configure triplet for release builds only and use overlay for lzo to prevent error with source builds

--- setup.py
+++ setup.py
@@ -77,5 +77,5 @@ setup(
     # ! setup_requires=["cmake"] should not be used, as it causes `vcpkg` to fail
     cmake_with_sdist=True,
     # Signal cmake to use `vcpkg`
-    cmake_args=["-DBUILD_TESTS=OFF", "-DVCPKG_BUILD_TYPE=Release", "-DCMAKE_BUILD_TYPE=Release"]
+    cmake_args=["-DBUILD_TESTS=OFF"],
 )
